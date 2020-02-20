a3eae51b8d114aa115fcb9a9c4c279040601313d
ishland <ishlandmc@yeah.net>
2020-02-20 14:27:27 +0800

    ForumSystem

--- src/main/java/red/mohist/xenforologin/interfaces/ForumSystem.java
+++ src/main/java/red/mohist/xenforologin/interfaces/ForumSystem.java
@@ -1,6 +1,21 @@
 package red.mohist.xenforologin.interfaces;
 
+import org.bukkit.entity.Player;
+import red.mohist.xenforologin.enums.ResultType;
+
+import javax.annotation.Nonnull;
+
 public interface ForumSystem {
 
+    boolean isAvailable();
+
+    @Nonnull
+    ResultType register(Player player, String password, String email);
+
+    @Nonnull
+    ResultType login(Player player, String password);
+
+    @Nonnull
+    ResultType join(Player player);
 
 }
