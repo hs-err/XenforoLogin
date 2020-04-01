1e87f5e18df0deabe256cfbb1b5a9ebc6cbf3b4a
ishland <ishlandmc@yeah.net>
2020-04-01 22:50:25 +0800

    Copyright and remove Fabric temporarily

--- XenforoLogin-Bukkit/src/main/java/red/mohist/xenforologin/bukkit/listeners/ListenerInventoryClickEvent.java
+++ XenforoLogin-Bukkit/src/main/java/red/mohist/xenforologin/bukkit/listeners/ListenerInventoryClickEvent.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of XenforoLogin, licensed under the GNU Lesser General Public License v3.0 (LGPLv3).
+ *
+ * Copyright (c) 2020 Mohist-Community.
+ *
+ */
+
 package red.mohist.xenforologin.bukkit.listeners;
 
 import org.bukkit.entity.Player;
