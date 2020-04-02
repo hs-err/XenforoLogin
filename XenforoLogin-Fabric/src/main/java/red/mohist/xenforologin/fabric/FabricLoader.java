269b0d3744fca99e665196751319f68527e1f1d2
ishland <ishlandmc@yeah.net>
2020-04-02 09:32:14 +0800

    Initial Fabric mod

---
+++ XenforoLogin-Fabric/src/main/java/red/mohist/xenforologin/fabric/FabricLoader.java
@@ -0,0 +1,17 @@
+/*
+ * This file is part of XenforoLogin, licensed under the GNU Lesser General Public License v3.0 (LGPLv3).
+ *
+ * Copyright (c) 2020 Mohist-Community.
+ *
+ */
+
+package red.mohist.xenforologin.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricLoader implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello world!");
+    }
+}
