b505a54e395032d5951c8948a433ef908c2b8687
ishland <ishlandmc@yeah.net>
2020-04-23 12:32:05 +0800

    Add YggdrasilServer

---
+++ XenforoLogin-YggdrasilServer/src/main/java/red/mohist/xenforologin/yggdrasilserver/YggdrasilServerEntry.java
@@ -0,0 +1,24 @@
+/*
+ * This file is part of XenforoLogin, licensed under the GNU Lesser General Public License v3.0 (LGPLv3).
+ *
+ * You are not permitted to interfere any protection that prevents loading in CatServer
+ *
+ * Copyright (c) 2020 Mohist-Community.
+ *
+ */
+
+package red.mohist.xenforologin.yggdrasilserver;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class YggdrasilServerEntry {
+    private static final Logger logger = LogManager
+            .getLogger("XenforoLogin-Yggdrasil-Launcher");
+
+
+    public static void main(String[] args) {
+        logger.info("Hello world!");
+    }
+
+}
