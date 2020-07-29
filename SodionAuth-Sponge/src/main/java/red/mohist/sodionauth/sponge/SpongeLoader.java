e6e82967aa58d87ae88a0a6bd8f21e04aaf9eaef
ishland <ishlandmc@yeah.net>
2020-07-29 08:50:25 +0800

    Back to SNAPSHOT

--- SodionAuth-Sponge/src/main/java/red/mohist/sodionauth/sponge/SpongeLoader.java
+++ SodionAuth-Sponge/src/main/java/red/mohist/sodionauth/sponge/SpongeLoader.java
@@ -48,7 +48,7 @@ import java.util.*;
         id = "sodionauth",
         name = "SodionAuth",
         description = "A new generation of authentication plugin for Minecraft",
-        version = "2.0-ALPHA.1"
+        version = "2.0-SNAOSHOT"
 )
 public class SpongeLoader implements PlatformAdapter {
     public static SpongeLoader instance;
